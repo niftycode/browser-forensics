51e1fc456c93e519446d6512f4dad03d0f3f8502
Bodo Schönfeld <bodo.schoenfeld@niftycode.de>
2019-05-21 10:41:54 +0200

    add epoch test

--- tests/test_epoch.py
+++ tests/test_epoch.py
@@ -4,12 +4,12 @@
 
 from browser_forensics import common_methods
 
-"""
+
 def test_convert_epoch_is_equal():
     timestamp = 1551173630212966
     rval = common_methods.convert_epoch(timestamp)
     assert rval == "Tue Feb 26 10:33:50 2019"
-"""
+
 
 def test_convert_epoch_is_not_equal():
     timestamp = 1551173629692660
